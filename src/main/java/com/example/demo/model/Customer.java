ce49b5f71ba15425d2fcce737002639147a0a8a8
weijianwei <weijianwei@mintq.com>
2018-06-27 15:47:31 +0800

    idea add Customer.java

---
+++ src/main/java/com/example/demo/model/Customer.java
@@ -0,0 +1,19 @@
+package com.example.demo.model;
+
+/**
+ * @author weijianwei
+ * @date 2018/6/27 15:44
+ * @description
+ */
+public class Customer {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
